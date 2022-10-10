35e77ec824054cbbfea8fb1d0bb5d811c9b5896a
IreenHinnawi <hinnawiireen@gmail.com>
2022-10-10 23:56:33 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+void main(){ //pre-defined function
+  //code
+  print("hello world"); //print in the debug console
+  String name = "ireen";
+  String fname = "Hinnawi";
+  int age = 12;
+  print(name);
+  print (age);
+  print("$name $fname");
+  double w =2;
+  print("$w");
+  bool r = true;
+  print(r);
+  String? name1 ;
+  print(name1);
+  int a = 4;
+  int b = 6;
+  int c = a + b ;
+  print(c);
+  print(a is int);
+  a ??=20; //if a is null make it 20
+  print(a);
+}
